6247360c8efb3a285c0edd9546cbe8d688af818e
Akshay Rathi <akshayrathi96@gmail.com>
2021-09-10 15:47:47 +0530

    File structure changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,12 +21,12 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-unused-vars': 'warn',
+    // '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-var-requires': 'off',
     'react/prop-types': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './GlobalStyle.scss';
 
 const App: React.FC = () => {
   return <div>This is the App Component !!!</div>;
