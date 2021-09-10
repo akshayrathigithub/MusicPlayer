4047668bab6332e0b143feec347d20a0aa449ab0
Akshay Rathi <akshayrathi96@gmail.com>
2021-09-11 00:37:23 +0530

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import SideBar from './Components/SideBar/SideBar';
 import './GlobalStyle.scss';
 
 const App: React.FC = () => {
-  return <div>This is the App Component !!!</div>;
+  return (
+    <div className="app">
+      <div className="side-bar">
+        <SideBar />
+      </div>
+      <div className="content"></div>
+    </div>
+  );
 };
 export default App;
