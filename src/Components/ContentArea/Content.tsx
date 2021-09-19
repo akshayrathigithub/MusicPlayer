57c870b024c13fbbd1565d23633eeb55c6376654
Akshay Rathi <akshayrathi96@gmail.com>
2021-09-19 23:53:53 +0530

    sliders added

--- src/Components/ContentArea/Content.tsx
+++ src/Components/ContentArea/Content.tsx
@@ -1,34 +1,115 @@
-import { useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import SearchInput from '../../SharedComponents/SearchInput/SearchInput';
 import './Content.scss';
 
 const Content: React.FC = () => {
-  const [flag, setflag] = useState(false);
+  const initialState = {
+    list: [
+      'yellow',
+      'green',
+      'palevioletred',
+      'purple',
+      'orange',
+      'red',
+      'blue',
+      'pink',
+      'white',
+    ],
+    activeIndex: 4,
+    middleIndex: 5,
+    totalLength: 8,
+  };
+  const classList: string[] = [
+    'third-right',
+    'second-right',
+    'center',
+    'second-left',
+    'third-left',
+  ];
+  const [contentState, setContentState] = useState(initialState);
+  useEffect(() => {
+    const updatedState = contentState;
+    const middleIndex = Number(updatedState.list.length / 2 - 0.5);
+    updatedState.middleIndex = middleIndex;
+    updatedState.activeIndex = middleIndex;
+    updatedState.totalLength = updatedState.list.length;
+    setContentState({ ...updatedState });
+    return () => {
+      // cleanup
+    };
+  }, []);
   return (
     <div className="content-wrapper">
       <div className="searchable-input">
         <SearchInput />
       </div>
       <div className="main-slider">
-        <div className="third-left" style={{ backgroundColor: 'yellow' }}></div>
-        <div
-          className={`third-right ${flag ? 'new-second-right' : ''}`}
-          style={{ backgroundColor: 'green' }}
-        ></div>
-        <div
-          className={`second-left ${flag ? 'new-second-left' : ''}`}
-          style={{ backgroundColor: 'palevioletred' }}
-        ></div>
-        <div
-          className={`second-right ${flag ? 'new-second-right' : ''}`}
-          style={{ backgroundColor: 'purple' }}
-        ></div>
-        <div
-          className={`center ${flag ? 'center-left' : ''}`}
-          style={{ backgroundColor: 'orange' }}
-        ></div>
+        {contentState.list.map((x, index) => {
+          let reactElement: ReactElement = <></>;
+          if (index === 0) {
+            reactElement = (
+              <div
+                className={`${classList[2]}`}
+                style={{
+                  backgroundColor: `${
+                    contentState.list[contentState.activeIndex]
+                  }`,
+                }}
+              ></div>
+            );
+          } else {
+            if (contentState.activeIndex - index >= 0) {
+              reactElement = (
+                <div
+                  className={
+                    index > 2 ? 'third-left' : `${classList[2 + index]}`
+                  }
+                  style={{
+                    backgroundColor: `${
+                      contentState.list[contentState.activeIndex + index]
+                    }`,
+                  }}
+                ></div>
+              );
+            }
+            if (contentState.activeIndex + index < contentState.totalLength) {
+              reactElement = (
+                <div
+                  className={
+                    index > 2 ? 'third-right' : `${classList[2 - index]}`
+                  }
+                  style={{
+                    backgroundColor: `${
+                      contentState.list[contentState.activeIndex - index]
+                    }`,
+                  }}
+                ></div>
+              );
+            }
+          }
+          return <React.Fragment key={index}>{reactElement}</React.Fragment>;
+        })}
       </div>
-      <button onClick={() => setflag(!flag)}>hello</button>
+      <button
+        onClick={() =>
+          setContentState({
+            ...contentState,
+            activeIndex: contentState.activeIndex - 1,
+          })
+        }
+      >
+        Left
+      </button>
+      <button
+        onClick={() =>
+          setContentState({
+            ...contentState,
+            activeIndex: contentState.activeIndex + 1,
+          })
+        }
+      >
+        Right
+      </button>
     </div>
   );
 };
