347c50146edf3357048f309138ce415eabac5b44
Akshay Rathi <akshayrathi96@gmail.com>
2021-09-26 22:30:54 +0530

    slider translate values added

--- src/Constants/Constant.ts
+++ src/Constants/Constant.ts
@@ -1,3 +1,31 @@
 export const COMMON_CONSTANT = {
   APP_NAME: 'Music Player',
 };
+
+export const SLIDER_TRANSLATE_VALUES = {
+  thirdLeft: {
+    transform: 'translate3d(-24rem, 0px, 0px)',
+    width: '40%',
+    height: '70%',
+  },
+  secondLeft: {
+    transform: 'translate3d(-19rem, 0px, 0px)',
+    width: '45%',
+    height: '75%',
+  },
+  center: {
+    transform: 'translate3d(0rem, 0px, 0px)',
+    width: '70%',
+    height: '90%',
+  },
+  secondRight: {
+    transform: 'translate3d(19rem, 0px, 0px)',
+    width: '45%',
+    height: '75%',
+  },
+  thirdRight: {
+    transform: 'translate3d(24rem, 0px, 0px)',
+    width: '40%',
+    height: '70%',
+  },
+};
