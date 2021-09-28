ab09c41c7e580365fd578a0dc1fdea8b139ce6dd
Akshay Rathi <akshayrathi96@gmail.com>
2021-09-28 23:50:59 +0530

    component modification added to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Content from './Components/ContentArea/Content';
+import Home from './Components/Home/Home';
 import SideBar from './Components/SideBar/SideBar';
 import './GlobalStyle.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar/NavBar';
 
 const App: React.FC = () => {
   return (
@@ -11,13 +12,14 @@ const App: React.FC = () => {
         <div className="side-bar">
           <SideBar />
         </div>
-        <Switch>
-          <Route exact path="/">
-            <div className="content">
-              <Content />
-            </div>
-          </Route>
-        </Switch>
+        <div className="content">
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
