b55163c7590486f52f0db09c88e573eaf0346ce4
Akshay Rathi <akshayrathi96@gmail.com>
2021-09-28 23:50:03 +0530

    search bar moved to navBar component

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SearchInput from '../../SharedComponents/SearchInput/SearchInput';
 
-function NavBar() {
-  return <div></div>;
-}
+const NavBar: React.FC = () => {
+  return (
+    <div className="nav-bar">
+      <div className="searchable-input">
+        <SearchInput />
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
